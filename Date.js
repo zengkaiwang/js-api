b7c4e98d14af7dc33191573e1509fbe411426c85
scaler <zengkaiwang@yeah.net>
2021-08-10 10:07:03 +0800

    Date

---
+++ Date.js
@@ -0,0 +1,14 @@
+let date = new Date()
+console.log(date)
+
+let year = date.getFullYear()
+var month = date.getMonth()
+var day = date.getDate()
+
+var week = date.getDay()
+console.log(week)
+
+var hour = date.getHours()
+var minutes = date.getMinutes()
+let second = date.getSeconds()
+console.log(year, month, day, hour, minutes,second)

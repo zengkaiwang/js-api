274fe67d33261ad67cfedbf9ba328678c1679776
scaler <zengkaiwang@yeah.net>
2021-08-23 22:35:13 +0800

    两数之和

--- src/algorithms/数组.js
+++ src/algorithms/数组.js
@@ -1,5 +1,55 @@
 /**
- * 1.合并两个有序数组
+ * 2. 两数之和
+ * 题目描述: 给定一个整数数组 nums 和一个目标值 target ，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ */
+var nums = [2, 7, 11, 15],
+  target = 9
+// 期望: 因为 nums[0] + nums[1] = 2 + 7 = 9 所以返回 [0, 1]
+
+/**
+ * 解法1: 暴力枚举
+ * 思路及算法: 最容易想到的方法是枚举数组中的每一个数 x，寻找数组中是否存在 target - x。
+ * 时间复杂度：O(N^2)
+ * 空间复杂度：O(1)
+ */
+const twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j]
+      }
+    }
+  }
+
+  return []
+}
+
+twoSum(nums, 9)
+
+/**
+ * 解法2: 哈希表
+ * 思路及算法: 注意到方法一的时间复杂度较高的原因是寻找 target - x 的时间复杂度过高。使用哈希表，可以将寻找 target - x 的时间复杂度降低到从 O(N) 降低到 O(1)。
+ */
+const twoSum = function (nums, target) {
+  let map = new Map()
+  let k
+  for (let i = 0; i < nums.length; i++) {
+    k = target - nums[i]
+    if (map.has(k)) {
+      return [map.get(k), i]
+    }
+    map.set(nums[i], i)
+  }
+  return []
+}
+
+var nums = [2, 7, 11, 15]
+console.log(twoSum(nums, 9))
+
+// =========================================1.合并两个有序数组======================================
+
+/**
+ * 1. 合并两个有序数组
  */
 var nums1 = [1, 2, 3, 0, 0, 0],
   m = 3
